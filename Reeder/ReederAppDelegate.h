849bb0664e4eaeed0f69281f8e8f1cbfc7cbc020
Phil Nichols <philipdnichols@gmail.com>
2014-08-07 16:58:47 -0400

    initial commit

---
+++ Reeder/ReederAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ReederAppDelegate.h
+//  Reeder
+//
+//  Created by Philip Nichols on 8/7/14.
+//  Copyright (c) 2014 Phil Nichols. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReederAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Reeder/ReederViewController.h
@@ -0,0 +1,13 @@
+//
+//  ReederViewController.h
+//  Reeder
+//
+//  Created by Philip Nichols on 8/7/14.
+//  Copyright (c) 2014 Phil Nichols. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReederViewController : UIViewController
+
+@end
